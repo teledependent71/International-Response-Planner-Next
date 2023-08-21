0b32eada53ec188be214eff34dd8df8612945248
teledependent71 <93117032+teledependent71@users.noreply.github.com>
2023-08-21 13:46:19 +0300

    Commit from TeleportHQ

---
+++ pages/authors/[id].js
@@ -0,0 +1,92 @@
+import React from 'react'
+import Head from 'next/head'
+
+import { DataProvider, Repeater } from '@teleporthq/thq-react-components'
+import PropTypes from 'prop-types'
+
+import authorsPageInitialPaths9345eResource from '../../resources/authors-page-initial-paths-9345e'
+import authorsPageInitialPropsC491cResource from '../../resources/authors-page-initial-props-c491c'
+
+const Authors = (props) => {
+  return (
+    <>
+      <div className="authors-container">
+        <Head>
+          <title>Authors - International Response Planner</title>
+          <meta
+            property="og:title"
+            content="Authors - International Response Planner"
+          />
+        </Head>
+        <DataProvider
+          renderSuccess={(AuthorsEntity) => (
+            <>
+              <div className="authors-container1">
+                <h1>{AuthorsEntity?.name}</h1>
+                <span>{AuthorsEntity?.id}</span>
+              </div>
+            </>
+          )}
+          initialData={props.authorsEntity}
+          persistDataDuringLoading={true}
+          key={props.page}
+        />
+      </div>
+      <style jsx>
+        {`
+          .authors-container {
+            width: 100%;
+            display: flex;
+            overflow: auto;
+            min-height: 100vh;
+            align-items: center;
+            flex-direction: column;
+          }
+          .authors-container1 {
+            gap: 12px;
+            width: 100%;
+            display: flex;
+            flex-direction: column;
+          }
+        `}
+      </style>
+    </>
+  )
+}
+
+Authors.defaultProps = {
+  authorsEntity: [],
+}
+
+Authors.propTypes = {
+  authorsEntity: PropTypes.array,
+}
+
+export default Authors
+
+export async function getStaticPaths() {
+  const response = await authorsPageInitialPaths9345eResource({})
+  return {
+    paths: (response?.data || []).map((item) => {
+      return {
+        params: {
+          id: (item?.id).toString(),
+        },
+      }
+    }),
+    fallback: 'blocking',
+  }
+}
+
+export async function getStaticProps(context) {
+  const response = await authorsPageInitialPropsC491cResource({
+    ...context?.params,
+  })
+  return {
+    props: {
+      authorsEntity: response?.data?.[0],
+      ...response?.meta?.pagination,
+    },
+    revalidate: 60,
+  }
+}
---
+++ pages/books/index.js
@@ -0,0 +1,86 @@
+import React from 'react'
+import Head from 'next/head'
+
+import { DataProvider, Repeater } from '@teleporthq/thq-react-components'
+import PropTypes from 'prop-types'
+
+import booksPageInitialProps5b85aResource from '../../resources/books-page-initial-props-5b85a'
+
+const Books1 = (props) => {
+  return (
+    <>
+      <div className="books1-container">
+        <Head>
+          <title>Books1 - International Response Planner</title>
+          <meta
+            property="og:title"
+            content="Books1 - International Response Planner"
+          />
+        </Head>
+        <DataProvider
+          renderSuccess={(params) => (
+            <>
+              <Repeater
+                items={params}
+                renderItem={(BooksEntities) => (
+                  <>
+                    <div className="books1-container1">
+                      <h1>{BooksEntities?.title}</h1>
+                      <span>{BooksEntities?.title}</span>
+                      <span>{BooksEntities?.price}</span>
+                    </div>
+                  </>
+                )}
+              />
+            </>
+          )}
+          initialData={props.booksEntities}
+          persistDataDuringLoading={true}
+          key={props.page}
+        />
+      </div>
+      <style jsx>
+        {`
+          .books1-container {
+            width: 100%;
+            display: flex;
+            overflow: auto;
+            min-height: 100vh;
+            align-items: center;
+            flex-direction: column;
+          }
+          .books1-container1 {
+            gap: 12px;
+            width: 100%;
+            display: flex;
+            align-items: center;
+            flex-direction: column;
+          }
+        `}
+      </style>
+    </>
+  )
+}
+
+Books1.defaultProps = {
+  booksEntities: [],
+}
+
+Books1.propTypes = {
+  booksEntities: PropTypes.array,
+}
+
+export default Books1
+
+export async function getStaticProps(context) {
+  const response = await booksPageInitialProps5b85aResource({
+    ...context?.params,
+  })
+  return {
+    props: {
+      booksEntities: response?.data,
+      ...response?.meta?.pagination,
+    },
+    revalidate: 60,
+  }
+}
